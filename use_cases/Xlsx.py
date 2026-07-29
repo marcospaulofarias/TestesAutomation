66ecc65c0496c77964a421aed6abe87e6e49f53a
Marcos Farias [ Zallpy ] <marcospaulo_farias@outlook.com>
2026-07-29 09:52:19 -0300

    refactor(xlsx): new params in functions and bug corrections

--- use_cases/Xlsx.py
+++ use_cases/Xlsx.py
@@ -1,41 +1,61 @@
 from loguru import logger
 import openpyxl # type: ignore
 from use_cases.PrintAutomation import PrintAutomation
+from os import path
 
 class Xlsx:
     def __init__(self, file_path: str):
         """Classe para manipulação de arquivos Excel (.xlsx) usando openpyxl.
-        :param file_path: Caminho completo do arquivo Excel a ser manipulado."""
+
+        :param file_path: Caminho completo do arquivo Excel a ser manipulado.
+        """
         self.file_path = file_path
+        if not path.exists(file_path):
+            self.workbook = openpyxl.Workbook()
+            self._save_workbook()
         self.workbook = openpyxl.load_workbook(file_path)
         self.printautomation = PrintAutomation()
 
     def get_sheet_names(self):
-        """Retorna uma lista com os nomes das planilhas no arquivo Excel."""
+        """Retorna uma lista com os nomes das planilhas no arquivo Excel.
+        
+        :return: Lista com os nomes das planilhas no arquivo.
+        """
         return self.workbook.sheetnames
     
-    def get_sheet(self, sheet_name: str):
+    def get_sheet(self, sheet_name: str, create: bool=False):
         """Retorna a planilha especificada pelo nome.
+
         :param sheet_name: Nome da planilha a ser retornada.
-        :return: Objeto da planilha."""
+        :param create: Criar planilha se não existir?
+        :return: Objeto da planilha.
+        """
         if sheet_name not in self.workbook.sheetnames:
+            if create:
+                self.workbook.create_sheet(sheet_name)
+                return self.workbook[sheet_name]
             raise ValueError(f"Sheet '{sheet_name}' does not exist in the workbook.")
         return self.workbook[sheet_name]
     
     def get_cell_value(self, sheet_name: str, cell_reference: str):
         """Retorna o valor da célula especificada.
+
         :param sheet_name: Nome da planilha que contém a célula.
         :param cell_reference: Referência da célula (ex: 'A1').
-        :return: Valor da célula."""
+        :return: Valor da célula.
+        """
         sheet = self.get_sheet(sheet_name)
         return sheet[cell_reference].value
     
-    def set_cell_value(self, sheet_name: str, cell_reference: str, value):
+    def set_cell_value(self, sheet_name: str, cell_reference: str, value: any, create: bool=False):
         """Define o valor da célula especificada.
+
         :param sheet_name: Nome da planilha que contém a célula.
         :param cell_reference: Referência da célula (ex: 'A1').
-        :param value: Valor a ser definido na célula."""
-        sheet = self.get_sheet(sheet_name)
+        :param value: Valor a ser definido na célula.
+        :param create: Criar planilha se não existir?
+        """
+        sheet = self.get_sheet(sheet_name, create=create)
         sheet[cell_reference].value = value
         self._save_workbook()
 
@@ -46,16 +66,3 @@ class Xlsx:
             logger.critical(f'Erro ao salvar o arquivo "{self.file_path}": {error_x}')
             self.printautomation.print_error()
             raise RuntimeError(f'Erro ao salvar o arquivo "{self.file_path}": {error_x}') from error_x
-
-
-if __name__ == '__main__':
-    # Exemplo de uso
-    xlsx = Xlsx("C:\\Zallpy\\PythonAutomationTemplate\\workbooks\\testes\\arquivo_excel.xlsx")
-    print(f"Workbook loaded from: {xlsx.file_path}")
-    print(f"Sheet names: {xlsx.get_sheet_names()}")
-    sheet = xlsx.get_sheet("Planilha1")
-    print(f"Accessing sheet: {sheet.title}")
-    cell_value = xlsx.get_cell_value("Planilha1", "A1")
-    print(f"Value in cell A1: {cell_value}")
-    xlsx.set_cell_value("Planilha1", "A1", "1")
-    print("Cell value updated.")
