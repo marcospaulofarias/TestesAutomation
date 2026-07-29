8dd8bc72974c4209bce340edb8760e71e5a4ba9b
Marcos Farias [ Zallpy ] <marcospaulo_farias@outlook.com>
2026-07-29 11:02:30 -0300

    Revert "refactor(date_time_utils): add new function to transform str date in tuple; docstring corrections"
    This reverts commit 22595baf2a27edd98a716ea29474beb36641c51c.

--- utils/date_time_utils.py
+++ utils/date_time_utils.py
@@ -2,17 +2,4 @@ import re
 from datetime import datetime
 
 def get_numeric_timestamp() -> str:
-    """Função para retornar a data e hora atual em forma de str, ex: 2026-07-27:00:00:00 -> 20260727000000.
-    
-    :return: str(20260727000000).
-    """
     return re.sub(r"\D", "", str(datetime.now()))
-
-def date_str_to_int(date_time_str: str) -> tuple:
-    """Função para transformar uma data "27/07/2026" -> (27, 7, 2026).
-    
-    :param date_time_str: Parâmetro de data formato str, ex: "27/07/2026".
-    :return: (27, 7, 2026).
-    """
-    date_split = date_time_str.split("/")
-    return int(date_split[0]), int(date_split[1]), int(date_split[2])
